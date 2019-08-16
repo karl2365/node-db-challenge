cad6da363a51e2a6018abded9a14c4a4b6e2c8b7
karl2365 <karlbeksa@gmail.com>
2019-08-16 11:34:25 -0400

    :construction: server running

---
+++ project/project-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const knex = require('knex');
+const project = require('./project-model.js');
+
+
+
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
