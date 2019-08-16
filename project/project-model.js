cc9c8e81f91347a9520bc27025718fc3822a761d
karl2365 <karlbeksa@gmail.com>
2019-08-16 13:42:02 -0400

    :boom: MVP Complete

--- project/project-model.js
+++ project/project-model.js
@@ -5,7 +5,10 @@ module.exports = {
 
     addResource,
     getResources,
-    addProject
+    addProject,
+    getProjects,
+    addTask,
+    getTasks
 };
 
 
@@ -18,5 +21,21 @@ function getResources() {
 }
 
 function addProject(project) {
+    console.log(project);
     return db('project').insert(project);
+}
+
+function getProjects() {
+    return  db('project');
+}
+
+function addTask(task) {
+    return db('task').insert(task);
+}
+
+function getTasks() {
+    return db('task as t')
+        .innerJoin('project as p', 't.project_id', 'p.id' )
+        .select('p.name', 'p.description', 't.description as task', 't.notes', 't.task_completed')
+        
 }
--- project/project-router.js
+++ project/project-router.js
@@ -23,17 +23,79 @@ router.get('/resource', async (req, res) => {
     try {
         const resources = await project.getResources();
         if (resources) {
-            res.json(resources);
+            res.status(200).json(resources);
           } else {
             res.status(404).json({ message: 'Could not find resources.' })
           }
     }
     catch (err) {
-        res.status(500).json({ message: 'Failed to get resources' });
+        res.status(500).json({ message: 'Failed to get resources.' });
       }
 } 
 );
 
+router.post('/', async (req, res) => {
+    const projectData = req.body;
+    try {
+        const projects = await project.addProject(projectData);
+        res.status(201).json(projects);
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Failed to add project.' });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await project.getProjects();
+        console.log(projects)
+        if (projects) {
+            for (let i = 0; i < projects.length; i++){
+                projects[i].project_completed === 0 ? projects[i].project_completed = false : projects[i].project_completed = true
+            }
+            res.status(200).json(projects);
+        }
+        else {
+            res.status(404).json({ message: 'Could not find projects.' })
+
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Failed to retrieve projects.' });
+
+    }
+});
+
+router.post('/tasks', async (req, res) => {
+    const taskData = req.body;
+    try {
+        const task = await project.addTask(taskData);
+        res.status(201).json(task);
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Failed to add task.' });
+    }
+});
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await project.getTasks();
+        if (tasks) {
+            for (let i = 0; i < tasks.length; i++){
+                tasks[i].task_completed === 0 ? tasks[i].task_completed = false : tasks[i].task_completed = true
+            }
+            res.status(200).json(tasks);
+        }
+        else {
+            res.status(404).json({ message: 'Could not find tasks.' })
+
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Failed to retrieve tasks.' });
+
+    }
+})
 
 
 module.exports = router;
